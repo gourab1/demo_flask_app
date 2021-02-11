686cc95fcab697be3080c5fab5eccfd8db9a88e2
EC2 Default User <ec2-user@ip-172-31-46-219.ec2.internal>
2021-02-11 16:54:01 +0000

    says works well

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "this works"
+    return "this works well!"
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
