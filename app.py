510854195924f2111012ed6a108723ac93b23b5d
EC2 Default User <ec2-user@ip-172-31-46-219.ec2.internal>
2021-02-13 15:31:03 +0000

    new app.py

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
-
 @app.route('/')
 def index():
-    return "this works well!"
+    return render_template('basic.html')
+    
+@app.route('/get')
+def get():
+    testapi = 'https://api.nal.usda.gov/fdc/v1/foods/search?api_key=DEMO_KEY&query=kraft%20mac%20cheese'
+    return requests.get(testapi).content
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
