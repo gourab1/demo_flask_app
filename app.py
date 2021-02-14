ef4310572a78a0f2c4f35939f7bb95f094d7868e
EC2 Default User <ec2-user@ip-172-31-46-219.ec2.internal>
2021-02-14 11:30:03 +0000

    forms for donation

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask, render_template
-import requests
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 @app.route('/')
 def index():
     name = 'Bani'
     return render_template('home.html', name=name)
-
+    
+@app.route('/form_donation')
+def form_donation():
+    name = 'Bani'
+    return render_template('form_donation.html', name=name)
+    
+@app.route('/thankyou')
+def thankyou():
+    name = 'Bani'
+    donation_item = request.args.get('donation_item')
+    return render_template('thankyou.html', name=name, donation_item=donation_item)
+    
 @app.route('/give')
 def give():
     name = 'Bani'
@@ -16,7 +26,7 @@ def give():
 @app.route('/get')
 def get():
     testapi = 'https://api.nal.usda.gov/fdc/v1/foods/search?api_key=DEMO_KEY&query=kraft%20mac%20cheese'
-    return requests.get(testapi).content
+    return request.get(testapi).content
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
